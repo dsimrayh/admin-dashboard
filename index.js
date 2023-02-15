0fdae470d448c5e345949c5cf7aded37bfccae03
Damian <dsimrayh@yahoo.com>
2023-02-14 17:48:15 -0800

    navbar mobile responsive
    updated the navbar to be responsive for mobile devices. discovered a bug with the JS that needs fixing (style issues once hamburger menu button is clicked and window is resized).

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ const search = document.querySelector('#search-bar');
 search.addEventListener('focusout', () => {
     search.value = '';
 });
+
+// Handles displaying / hiding navbar when menu icons are clicked
+const menuIcon = document.querySelector('#menu-icon');
+const menuIcon2 = document.querySelector('#menu-icon-2')
+const navbar = document.querySelector('#navbar');
+const container = document.querySelector('.container');
+// Display navbar, have it take up the whole screen, hide the overflow
+menuIcon.addEventListener('click', () => {
+    container.style.gridTemplateRows = '100vh';
+    container.style.gridTemplateAreas = '"navbar"';
+    container.style.overflow = 'hidden';
+    navbar.style.display = 'flex';
+})
+// Revert back to standard styles for mobile and hide the navbar again
+menuIcon2.addEventListener('click', () => {
+    container.style.gridTemplateRows = 'auto 30vh 1fr';
+    container.style.gridTemplateAreas = '"menu" "header" "main"';
+    container.style.overflow = 'visible';
+    navbar.style.display = 'none';
+})
